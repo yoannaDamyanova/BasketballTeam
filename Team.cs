c8327bd9f1b930b775aab7099eb7846b69a66486
Yoanna Damyanova <133016964+yoannaDamyanova@users.noreply.github.com>
2024-03-05 08:42:34 +0200

    Create Team.cs

---
+++ Team.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreatingABasketballTeamJSON
+{
+    public class Team
+    {
+        public Team(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public double Rating
+        {
+            get
+            {
+                if (Players.Count == 0)
+                {
+                    return 0;
+                }
+
+                double rating = 0;
+                Players.ForEach(player => rating += player.AverageStats);
+
+                return Math.Round(rating);
+            }
+        }
+
+        public List<Player> Players { get; set; } = new List<Player>();
+
+        public void AddPlayer(Player player)
+        {
+            Players.Add(player);
+        }
+
+        public void RemovePlayer(Player player)
+        {
+            Players.Remove(player);
+        }
+    }
+}
